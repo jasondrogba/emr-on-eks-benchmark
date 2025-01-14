fd82dcff73e39f901595163d0573fcc54545578d
jasondrogba <757104667@qq.com>
2025-01-14 20:03:24 +0800

    reduce provision

--- provision.sh
+++ provision.sh
@@ -19,50 +19,50 @@ echo "==============================================="
 echo "  setup IAM roles ......"
 echo "==============================================="
 
-# create S3 bucket for application
-if [ $AWS_REGION=="us-east-1" ]; then
-  aws s3api create-bucket --bucket $S3TEST_BUCKET --region $AWS_REGION
-else
-  aws s3api create-bucket --bucket $S3TEST_BUCKET --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
-fi
-# Create a job execution role (https://docs.aws.amazon.com/emr/latest/EMR-on-EKS-DevelopmentGuide/creating-job-execution-role.html)
-cat >/tmp/job-execution-policy.json <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": ["s3:PutObject","s3:DeleteObject","s3:GetObject","s3:ListBucket"],
-            "Resource": [
-              "arn:aws:s3:::${S3TEST_BUCKET}",
-              "arn:aws:s3:::${S3TEST_BUCKET}/*",
-              "arn:aws:s3:::blogpost-sparkoneks-us-east-1/blog/BLOG_TPCDS-TEST-3T-partitioned/*",
-              "arn:aws:s3:::blogpost-sparkoneks-us-east-1"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [ "logs:PutLogEvents", "logs:CreateLogStream", "logs:DescribeLogGroups", "logs:DescribeLogStreams", "logs:CreateLogGroup" ],
-            "Resource": [ "arn:aws:logs:*:*:*" ]
-        }
-    ]
-}
-EOF
+## create S3 bucket for application
+#if [ $AWS_REGION=="us-east-1" ]; then
+#  aws s3api create-bucket --bucket $S3TEST_BUCKET --region $AWS_REGION
+#else
+#  aws s3api create-bucket --bucket $S3TEST_BUCKET --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+#fi
+## Create a job execution role (https://docs.aws.amazon.com/emr/latest/EMR-on-EKS-DevelopmentGuide/creating-job-execution-role.html)
+#cat >/tmp/job-execution-policy.json <<EOF
+#{
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Effect": "Allow",
+#            "Action": ["s3:PutObject","s3:DeleteObject","s3:GetObject","s3:ListBucket"],
+#            "Resource": [
+#              "arn:aws:s3:::${S3TEST_BUCKET}",
+#              "arn:aws:s3:::${S3TEST_BUCKET}/*",
+#              "arn:aws:s3:::blogpost-sparkoneks-us-east-1/blog/BLOG_TPCDS-TEST-3T-partitioned/*",
+#              "arn:aws:s3:::blogpost-sparkoneks-us-east-1"
+#            ]
+#        },
+#        {
+#            "Effect": "Allow",
+#            "Action": [ "logs:PutLogEvents", "logs:CreateLogStream", "logs:DescribeLogGroups", "logs:DescribeLogStreams", "logs:CreateLogGroup" ],
+#            "Resource": [ "arn:aws:logs:*:*:*" ]
+#        }
+#    ]
+#}
+#EOF
 
-cat >/tmp/trust-policy.json <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [ {
-      "Effect": "Allow",
-      "Principal": { "Service": "eks.amazonaws.com" },
-      "Action": "sts:AssumeRole"
-    } ]
-}
-EOF
-
-aws iam create-policy --policy-name $ROLE_NAME-policy --policy-document file:///tmp/job-execution-policy.json
-aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file:///tmp/trust-policy.json
-aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::$ACCOUNTID:policy/$ROLE_NAME-policy
+#cat >/tmp/trust-policy.json <<EOF
+#{
+#  "Version": "2012-10-17",
+#  "Statement": [ {
+#      "Effect": "Allow",
+#      "Principal": { "Service": "eks.amazonaws.com" },
+#      "Action": "sts:AssumeRole"
+#    } ]
+#}
+#EOF
+#
+#aws iam create-policy --policy-name $ROLE_NAME-policy --policy-document file:///tmp/job-execution-policy.json
+#aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file:///tmp/trust-policy.json
+#aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::$ACCOUNTID:policy/$ROLE_NAME-policy
 
 echo "==============================================="
 echo "  Create EKS Cluster ......"
@@ -112,14 +112,14 @@ managedNodeGroups:
     volumeSize: 20
     volumeType: gp2
     minSize: 1
-    desiredCapacity: 1
+    desiredCapacity: 3
     maxSize: 30
     labels:
       app: sparktest 
-    tags:
+#    tags:
       # required for cluster-autoscaler auto-discovery
-      k8s.io/cluster-autoscaler/enabled: "true"
-      k8s.io/cluster-autoscaler/$EKSCLUSTER_NAME: "owned"  
+#      k8s.io/cluster-autoscaler/enabled: "true"
+#      k8s.io/cluster-autoscaler/$EKSCLUSTER_NAME: "owned"
 
 # enable all of the control plane logs
 cloudWatch:
@@ -133,94 +133,94 @@ eksctl create cluster -f /tmp/ekscluster.yaml
 # eksctl create nodegroup -f /tmp/ekscluster.yaml
 aws eks update-kubeconfig --name $EKSCLUSTER_NAME --region $AWS_REGION
 
-echo "==============================================="
-echo "  Enable EMR on EKS ......"
-echo "==============================================="
+#echo "==============================================="
+#echo "  Enable EMR on EKS ......"
+#echo "==============================================="
+#
+## Create kubernetes namespace for EMR on EKS
+#kubectl create namespace $EMR_NAMESPACE
+#
+## Enable cluster access for Amazon EMR on EKS in the 'emr' namespace
+#eksctl create iamidentitymapping --cluster $EKSCLUSTER_NAME --namespace $EMR_NAMESPACE --service-name "emr-containers"
+#aws emr-containers update-role-trust-policy --cluster-name $EKSCLUSTER_NAME --namespace $EMR_NAMESPACE --role-name $ROLE_NAME
+#
+## Create emr virtual cluster
+#aws emr-containers create-virtual-cluster --name $EMRCLUSTER_NAME \
+#  --container-provider '{
+#        "id": "'$EKSCLUSTER_NAME'",
+#        "type": "EKS",
+#        "info": { "eksInfo": { "namespace": "'$EMR_NAMESPACE'" } }
+#    }'
 
-# Create kubernetes namespace for EMR on EKS
-kubectl create namespace $EMR_NAMESPACE
+#echo "==============================================="
+#echo "  Configure EKS Cluster ......"
+#echo "==============================================="
+## config k8s rbac access to service account 'oss'
+#cat <<EOF | kubectl apply -f - -n $OSS_NAMESPACE
+#kind: Role
+#apiVersion: rbac.authorization.k8s.io/v1
+#metadata:
+#  name: $OSS_SPARK_SVCACCT_NAME-role
+#  namespace: $OSS_NAMESPACE
+#rules:
+#  - apiGroups: ["", "batch","extensions"]
+#    resources: ["configmaps","serviceaccounts","events","pods","pods/exec","pods/log","pods/portforward","secrets","services"]
+#    verbs: ["create","delete","get","list","patch","update","watch"]
+#---
+#kind: RoleBinding
+#apiVersion: rbac.authorization.k8s.io/v1
+#metadata:
+#  name: $OSS_SPARK_SVCACCT_NAME-rb
+#  namespace: $OSS_NAMESPACE
+#subjects:
+#  - kind: ServiceAccount
+#    name: $OSS_SPARK_SVCACCT_NAME
+#    namespace: $OSS_NAMESPACE
+#roleRef:
+#  kind: Role
+#  name: $OSS_SPARK_SVCACCT_NAME-role
+#  apiGroup: rbac.authorization.k8s.io
+#EOF
+#
+## Map S3 bucket to the EMR namespace dyanmically
+#kubectl create configmap --namespace $OSS_NAMESPACE special-config --from-literal=codeBucket=$S3TEST_BUCKET
+## kubectl create configmap --namespace $OSS_NAMESPACE pod-template --from-file=docker/benchmark-util/default-driver-pod-template.yaml --from-file=docker/benchmark-util/default-executor-pod-template.yaml
+#
+## Install k8s metrics server
+#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
-# Enable cluster access for Amazon EMR on EKS in the 'emr' namespace
-eksctl create iamidentitymapping --cluster $EKSCLUSTER_NAME --namespace $EMR_NAMESPACE --service-name "emr-containers"
-aws emr-containers update-role-trust-policy --cluster-name $EKSCLUSTER_NAME --namespace $EMR_NAMESPACE --role-name $ROLE_NAME
+## Install Cluster Autoscale that automatically adjusts the number of nodes in EKS
+#cat <<EOF >/tmp/autoscaler-config.yaml
+#---
+#autoDiscovery:
+#    clusterName: $EKSCLUSTER_NAME
+#awsRegion: $AWS_REGION
+#image:
+#    tag: v1.26.3
+#nodeSelector:
+#    app: sparktest
+#podAnnotations:
+#    cluster-autoscaler.kubernetes.io/safe-to-evict: 'false'
+#extraArgs:
+#    skip-nodes-with-system-pods: false
+#    scale-down-unneeded-time: 2m
+#    scale-down-unready-time: 5m
+#rbac:
+#    serviceAccount:
+#        create: false
+#        name: cluster-autoscaler
+#EOF
 
-# Create emr virtual cluster
-aws emr-containers create-virtual-cluster --name $EMRCLUSTER_NAME \
-  --container-provider '{
-        "id": "'$EKSCLUSTER_NAME'",
-        "type": "EKS",
-        "info": { "eksInfo": { "namespace": "'$EMR_NAMESPACE'" } }
-    }'
-
-echo "==============================================="
-echo "  Configure EKS Cluster ......"
-echo "==============================================="
-# config k8s rbac access to service account 'oss'
-cat <<EOF | kubectl apply -f - -n $OSS_NAMESPACE
-kind: Role
-apiVersion: rbac.authorization.k8s.io/v1
-metadata:
-  name: $OSS_SPARK_SVCACCT_NAME-role
-  namespace: $OSS_NAMESPACE
-rules:
-  - apiGroups: ["", "batch","extensions"]
-    resources: ["configmaps","serviceaccounts","events","pods","pods/exec","pods/log","pods/portforward","secrets","services"]
-    verbs: ["create","delete","get","list","patch","update","watch"]
----
-kind: RoleBinding
-apiVersion: rbac.authorization.k8s.io/v1
-metadata:
-  name: $OSS_SPARK_SVCACCT_NAME-rb
-  namespace: $OSS_NAMESPACE
-subjects:
-  - kind: ServiceAccount
-    name: $OSS_SPARK_SVCACCT_NAME
-    namespace: $OSS_NAMESPACE
-roleRef:
-  kind: Role
-  name: $OSS_SPARK_SVCACCT_NAME-role
-  apiGroup: rbac.authorization.k8s.io  
-EOF
-
-# Map S3 bucket to the EMR namespace dyanmically
-kubectl create configmap --namespace $OSS_NAMESPACE special-config --from-literal=codeBucket=$S3TEST_BUCKET
-# kubectl create configmap --namespace $OSS_NAMESPACE pod-template --from-file=docker/benchmark-util/default-driver-pod-template.yaml --from-file=docker/benchmark-util/default-executor-pod-template.yaml
-
-# Install k8s metrics server
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-
-# Install Cluster Autoscale that automatically adjusts the number of nodes in EKS
-cat <<EOF >/tmp/autoscaler-config.yaml
----
-autoDiscovery:
-    clusterName: $EKSCLUSTER_NAME
-awsRegion: $AWS_REGION
-image:
-    tag: v1.26.3
-nodeSelector:
-    app: sparktest    
-podAnnotations:
-    cluster-autoscaler.kubernetes.io/safe-to-evict: 'false'
-extraArgs:
-    skip-nodes-with-system-pods: false
-    scale-down-unneeded-time: 2m
-    scale-down-unready-time: 5m
-rbac:
-    serviceAccount:
-        create: false
-        name: cluster-autoscaler
-EOF
-
-helm repo add autoscaler https://kubernetes.github.io/autoscaler
-helm install nodescaler autoscaler/cluster-autoscaler --namespace kube-system --values /tmp/autoscaler-config.yaml --debug
-
-# Install Spark-Operator for the OSS Spark test
-helm repo add spark-operator https://kubeflow.github.io/spark-operator
-helm repo update
-helm install spark-operator spark-operator/spark-operator --version 1.4.6 \
-   --namespace $OSS_NAMESPACE \
-   --create-namespace \
-   --set webhook.enable=true
+#helm repo add autoscaler https://kubernetes.github.io/autoscaler
+#helm install nodescaler autoscaler/cluster-autoscaler --namespace kube-system --values /tmp/autoscaler-config.yaml --debug
+#
+## Install Spark-Operator for the OSS Spark test
+#helm repo add spark-operator https://kubeflow.github.io/spark-operator
+#helm repo update
+#helm install spark-operator spark-operator/spark-operator --version 1.4.6 \
+#   --namespace $OSS_NAMESPACE \
+#   --create-namespace \
+#   --set webhook.enable=true
 
 #echo "============================================================================="
 #echo "  Upload project examples to S3 ......"
